8b9744f60c70cfa34284e42803cbfff208f79db8
Alicia Courtois <alicia.courtois22@gmail.com>
2026-03-30 14:38:53 +0200

    supression de base de donnée et ajout de la complete

--- db/campus_helpdesk.sql
+++ db/campus_helpdesk.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Base de données :  `campus_helpdesk`
 --
 
+CREATE DATABASE IF NOT EXISTS campus_helpdesk;
+USE campus_helpdesk;
+
 -- --------------------------------------------------------
 
 --
